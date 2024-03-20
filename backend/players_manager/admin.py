6f89dec90cbbf27b1f61233c5812addeb479319c
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2024-03-20 13:26:03 +0100

    status added to players model

--- backend/players_manager/admin.py
+++ backend/players_manager/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from players_manager.models import Player
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'login', 'nickname', 'score', 'avatar')
+    list_display = ('id', 'login', 'nickname', 'score', 'status', 'avatar')
 
 admin.site.register(Player, PlayerAdmin)
 
--- backend/players_manager/models.py
+++ backend/players_manager/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Player (models.Model):
+	class status(models.TextChoices):
+		ONLINE = "ONLINE"
+		OFFLINE = "OFFLINE"
+		PLAYING = "PLAYING"
 	login = models.CharField(max_length=200)
 	password = models.CharField(max_length=200)
 	nickname = models.CharField(max_length=200)
@@ -12,4 +16,6 @@ class Player (models.Model):
 	nb_games_4p_won = models.IntegerField(default=0)
 	nb_games_4p_lost = models.IntegerField(default=0)
 	score = models.IntegerField(default=0)
+	status = models.CharField(max_length=200, choices=status.choices, default=status.OFFLINE)
+
 	avatar = models.ImageField(max_length=200, null=True, blank=True, upload_to='avatars/')
