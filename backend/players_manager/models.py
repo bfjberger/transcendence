658db85bf89ed8804640f4295de00aa607cf4763
Benoit Berger <bberger@student.42lausanne.ch>
2024-03-28 18:30:34 +0100

    superuser has to be created cleaned up

--- backend/players_manager/models.py
+++ backend/players_manager/models.py
@@ -1,25 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
-#class PlayerSuperUserCreator(BaseUserManager):
-
-#	def create_user(self, email, password=None, **extra_fields):
-#			email = self.normalize_email(email)
-#		user = self.model(email=email, **extra_fields)
-#			user.set_password(password)
-#			user.save(using=self._db)
-#			return user
-
-#	def create_superuser(self, username, password, **extra_fields):
-#		extra_fields.setdefault('is_staff', True)
-#		extra_fields.setdefault('is_superuser', True)
-#		extra_fields.setdefault('is_active', True)
-#		extra_fields.setdefault('is_admin', True)
-#		return self.create_user(username, password, **extra_fields)
-
-
-# creating a models Player, which inherite dj User class (AbstractUser)
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 	
 class PlayerSuperUser(BaseUserManager):
@@ -31,7 +11,7 @@ class PlayerSuperUser(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+# watch out create_user is used in the create_superuser function
     def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -40,8 +20,9 @@ class PlayerSuperUser(BaseUserManager):
         user.save(using=self._db)
         return user
 	
-
-class Player(AbstractBaseUser, PermissionsMixin):
+# we a use Player Class, which inherite AbstractUser to make a custom User Class
+# User class is needed to make tokens for exemple
+class Player(AbstractUser):
 	USERNAME_FIELD = 'username'
 	REQUIRED_FIELDS = []
 	
@@ -71,24 +52,3 @@ class Player(AbstractBaseUser, PermissionsMixin):
 	score = models.IntegerField(default=0)
 	status = models.CharField(max_length=200, choices=status.choices, default=status.OFFLINE)
 	avatar = models.ImageField(max_length=200, null=True, blank=True, upload_to='avatars/')
-
-
-# class Player (models.Model):
-	# class status(models.TextChoices):
-	# 	ONLINE = "ONLINE"
-	# 	OFFLINE = "OFFLINE"
-	# 	PLAYING = "PLAYING"
-	# login = models.CharField(max_length=200)
-	# password = models.CharField(max_length=200)
-	# nickname = models.CharField(max_length=200)
-	# nb_games_2p = models.IntegerField(default=0)
-	# nb_games_2p_won = models.IntegerField(default=0)
-	# nb_games_2p_lost = models.IntegerField(default=0)
-	# nb_games_4p = models.IntegerField(default=0)
-	# nb_games_4p_won = models.IntegerField(default=0)
-	# nb_games_4p_lost = models.IntegerField(default=0)
-	# score = models.IntegerField(default=0)
-	# status = models.CharField(max_length=200, choices=status.choices, default=status.OFFLINE)
-	# avatar = models.ImageField(max_length=200, null=True, blank=True, upload_to='avatars/')
-	# # def set_password(self, password):
-	# # 	self.password = password
