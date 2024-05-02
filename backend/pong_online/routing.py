cf0847d210b76f7c53eb2d52b529ff81a790774e
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2024-05-02 17:56:51 +0200

    #15 test passed for multi player online game with https

--- backend/pong_online/routing.py
+++ backend/pong_online/routing.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
 
-from .consumers import GameConsumer
-from .consumers_four import GameConsumerFour
+from .consumers import RoomConsumer
+from .consumers_four import RoomConsumerFour
 
 websocket_urlpatterns = [
-	re_path(r"wss/gameTwo/$", GameConsumer.as_asgi()),
-	re_path(r"wss/gameFour/$", GameConsumerFour.as_asgi()),
+	re_path(r"wss/gameTwo/$", RoomConsumer.as_asgi()),
+	re_path(r"wss/gameFour/$", RoomConsumerFour.as_asgi()),
 ]
