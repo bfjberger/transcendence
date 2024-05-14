03fb570558f3768306fc9c285e8931790a794ac1
Benoit Berger <bberger@student.42lausanne.ch>
2024-05-14 13:56:40 +0200

    #4 timer ok, att. colisions

--- backend/pong_IA/gamelogic.py
+++ backend/pong_IA/gamelogic.py
@@ -1,4 +1,4 @@
-import random, math
+import random, math, time
 
 from asgiref.sync import sync_to_async
 
@@ -24,8 +24,8 @@ class GameState:
 		self.winning_score = 2
 
 	def add_player(self):
-		self.player = self.GameState_player("player_left")
-		self.bot = self.GameState_player("player_right")
+		self.player = self.GameState_player()
+		self.bot = self.GameState_bot()
 
 	def remove_player_from_dict(self):
 		del self.player
@@ -49,30 +49,31 @@ class GameState:
 		if self.ball.x - (self.ball.radius / 2) <= 0:
 			await self.bot.score_point()
 			await self.ball.reset()
+			self.bot.flag = True
+			self.bot.future_ball_y = GAME_AREA_HEIGHT / 2
 		elif self.ball.x + (self.ball.radius / 2) >= GAME_AREA_WIDTH:
 			await self.player.score_point()
 			await self.ball.reset()
+			self.bot.flag = True
+			self.bot.future_ball_y = GAME_AREA_HEIGHT / 2
 
 		if self.player.score >= self.winning_score or self.bot.score >= self.winning_score:
 				self.is_running = False
 
 	async def update(self):
 		await self.player.move()
-		await self.bot.move()	
 		await self.ball.move(self.player, self.bot)
+		await self.bot.move(self.ball, self.player)
 		await self.handle_scores()
 
 	class GameState_player:
-		def __init__(self, position):
-			if position == 'player_left':
-				self.x = PADDLE_WIDTH
-			else:
-				self.x = GAME_AREA_WIDTH - PADDLE_WIDTH - 10
-
+		def __init__(self):
+			self.x = PADDLE_WIDTH
 			self.y = PLAYER_START_POS_Y
 			self.score = 0
 			self.is_moving = False
 			self.vertical = False
+			self.hits = 0
 
 		def check_bounds(self, y_position):
 			return y_position < 0 or y_position > GAME_AREA_HEIGHT - PADDLE_HEIGHT
@@ -89,7 +90,62 @@ class GameState:
 		async def score_point(self):
 			self.score += 1
 
+	class GameState_bot:
+		def __init__(self):
+			self.x = GAME_AREA_WIDTH - PADDLE_WIDTH - 10
+			self.y = PLAYER_START_POS_Y
+			self.score = 0
+			self.is_moving = False
+			self.vertical = False
+			self.hits = 0
+			self.future_ball_y = GAME_AREA_HEIGHT / 2
+			self.flag = True
+			self.timer_ai = time.time()
+
+		def check_bounds(self, y_position):
+			return y_position < 0 or y_position > GAME_AREA_HEIGHT - PADDLE_HEIGHT
+		
+		def future_ball_y_calcul(self, ball, player):
+			if time.time() - self.timer_ai >= 1:
+				self.future_ball_y = ball.y + ball.y_vel * (610/ball.x_vel) #610 = 650 - (10 + 10)*2, 650 wind lenght, 10 paddle length, 10 space between paddle and win, two times because two paddles 
+				self.flag = False
+				self.timer_ai = time.time()
+
+		async def move(self, ball, player):						
+			# Predictive movement when the ball start on ai side
+			x_vel = ball.x_vel
+			if x_vel > 0 and self.flag == True: 
+				self.future_ball_y = ball.y + ball.y_vel * (305/ball.x_vel) #295 = 650/2 - 10 - 10, screen width/2 - space between paddle and window limit - paddle width
+				self.flag = False
+			# management of future_ball_y, while the is touching the selling or the floor
+			while self.future_ball_y < 0 or self.future_ball_y > GAME_AREA_HEIGHT:
+				if self.future_ball_y < 0:
+					self.future_ball_y *= -1
+				elif self.future_ball_y > GAME_AREA_HEIGHT:
+					self.future_ball_y = GAME_AREA_HEIGHT - self.future_ball_y + GAME_AREA_HEIGHT
+							
 			
+			if self.future_ball_y < self.y :
+				self.move_paddle(up=True)
+			elif self.future_ball_y > (self.y + 70): # the origin of paddle is top left, though +70 pixels
+				self.move_paddle(up=False)
+
+		# check if the paddle is in the window and calls its derive
+		def move_paddle(self, up=True):
+			if up and self.y - PLAYER_SPEED < 0:
+				return False
+			if not up and self.y + PLAYER_SPEED > GAME_AREA_HEIGHT:
+				return False
+			self.move_prime(up)
+		# move paddle derive 
+		def move_prime(self, up=True):
+			if up:
+				self.y -= PLAYER_SPEED
+			else:
+				self.y += PLAYER_SPEED
+
+		async def score_point(self):
+			self.score += 1
 
 	class Ball:
 		def __init__(self):
@@ -105,7 +161,7 @@ class GameState:
 			self.x_vel = (math.cos(random_angle) * self.speed) * direction
 			self.y_vel = math.sin(random_angle) * self.speed
 
-		def handle_collision(self, player_left, player_right):
+		def handle_collision(self, player_left, bot):
 			if self.y - self.radius <= 0 or self.y + self.radius >= GAME_AREA_HEIGHT:
 				self.y_vel *= -1 * self.speed_multiplier_y
 				self.x_vel *= self.speed_multiplier_x
@@ -124,19 +180,22 @@ class GameState:
 						reduction_factor = PADDLE_HEIGHT / 2
 						new_y_vel = difference_in_y / reduction_factor
 						self.y_vel = -1 * new_y_vel
+						player_left.hits += 1
+						bot.future_ball_y_calcul(self, player_left)
 			elif self.x_vel > 0:
-				if (self.y <= player_right.y + PADDLE_HEIGHT and
-					self.y >= player_right.y and self.x < player_right.x and
-					self.x + self.radius >= player_right.x):
+				if (self.y <= bot.y + PADDLE_HEIGHT and
+					self.y >= bot.y and self.x < bot.x and
+					self.x + self.radius >= bot.x):
 						self.x_vel *= -1 * self.speed_multiplier_x
-						middle_y = player_right.y + PADDLE_HEIGHT / 2
+						middle_y = bot.y + PADDLE_HEIGHT / 2
 						difference_in_y = middle_y - self.y
 						reduction_factor = PADDLE_HEIGHT / 2
 						new_y_vel = difference_in_y / reduction_factor
 						self.y_vel = -1 * new_y_vel
+						bot.hits += 1
 
-		async def move(self, player_left, player_right):
-			self.handle_collision(player_left, player_right)
+		async def move(self, player_left, bot):
+			self.handle_collision(player_left, bot)
 			self.x += self.x_vel
 			self.y += self.y_vel
 			# print("ball x: ", self.x, "  |  ball y: ", self.y)
