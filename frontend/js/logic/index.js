290121b6cef1fcb072f4c76eb0b31d4dbcd7ff80
Amelie Snow <amsenaud@gmial.com>
2024-05-21 16:20:44 +0200

    Bug #45: Images de la page de garde

--- frontend/js/logic/index.js
+++ frontend/js/logic/index.js
@@ -3,14 +3,14 @@ import { connect_socket_friend } from "./friends.js";
 function renderIndex() {
 	return `
 			<div class="text-center py-5">
-				Bienvenue, ceci est la page principale.
+				<h1>Bienvenue, ceci est la page principale.</h1>
 			</div>
 			<div class="row">
 				<div class="col-6 d-flex justify-content-center">
-					<img src="/frontend/img/index42.png" alt="PONG X 42" width="600" height="600" />
+					<img src="/frontend/img/index42.png" alt="PONG X 42" style="max-width=600px; max-height=600px;" width=100% height=100% />
 				</div>
 				<div class="col-6 d-flex justify-content-center">
-					<img src="/frontend/img/index42_second.png" alt="PONG x 42" width="600" height="600"/>
+					<img src="/frontend/img/index42_second.png" alt="PONG x 42" style="max-width=600px; max-height=600px;" width=100% height=100%/>
 				</div>
 			</div>
 	`;
