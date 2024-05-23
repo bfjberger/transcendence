51e01ed5e1742466437af9740b0af290dcd46ba4
Killian Morin <killiancham@gmail.com>
2024-05-23 12:10:23 +0200

    #84 Modifier le pattern de la websocket pour les amis et ajouter des checks a la creation de compte (interdit le @ et +) | enlever le countdown avant la redirection vers la page de tournoi lors des tournois en ligne et en local

--- backend/friends_manager/routing.py
+++ backend/friends_manager/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 friends_websocket_urlpatterns = [
-    re_path(r'wss/notifications/(?P<username>\w+)/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'wss/notifications/(?P<username>[a-zA-Z0-9._-]+)/$', consumers.NotificationConsumer.as_asgi()),
 ]
